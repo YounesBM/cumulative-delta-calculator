54413c59d9c91fd2bf1515fda9a53be5b8748c22
YounesBM <you.benmaamar@gmail.com>
2023-06-26 05:53:56 +0200

    fixed typescript compilation issues with babel

--- dist/exchanges/symbol.js
+++
@@ -1,9 +0,0 @@
-export class Symbol {
-    constructor(symbol, name, baseCurrency, quoteCurrency) {
-        this.symbol = symbol;
-        this.name = name;
-        this.baseCurrency = baseCurrency;
-        this.quoteCurrency = quoteCurrency;
-    }
-}
-//# sourceMappingURL=symbol.js.map
--- src/test/exchangeController.integration.test.ts
+++ src/test/exchangeController.integration.test.ts
@@ -1,4 +1,4 @@
-import * as request from "supertest";
+import request from "supertest";
 import App from "../app";
 import ExchangeController from "../controllers/exchange.controller";
 import { APIFactory } from "../api/apiFactory";
